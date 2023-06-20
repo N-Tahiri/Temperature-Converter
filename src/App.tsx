956af5601d4826c2dadc29a12c149fbe4e8663ba
N.Tahiri <email@email.com>
2023-06-20 04:30:14 +0200

    finalized

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,46 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import TextField from "@mui/material/TextField";
-import { FactCheckRounded } from "@mui/icons-material";
 
 // Material UI
-
 function App() {
   const [celsius, setCelsius] = useState(0);
-  const [fahrenheit, setFahrenheit] = useState(0);
-  const [kelvin, setKelvin] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(32);
+  const [kelvin, setKelvin] = useState(273.15);
+
+  function handleChange(event) {
+    if (event.target.id === "celsius") {
+      const celsius = event.target.value;
+      setCelsius(celsius);
+
+      const toFahrenheit = (celsius * 9) / 5 + 32;
+      setFahrenheit(toFahrenheit);
+
+      const toKelvin = parseFloat(celsius) + 273.15;
+      setKelvin(toKelvin);
+    }
+    if (event.target.id === "fahrenheit") {
+      const fahrenheit = event.target.value;
+      setFahrenheit(fahrenheit);
+
+      const toCelsius = ((fahrenheit - 32) * 5) / 9;
+      setCelsius(toCelsius);
+
+      const toKelvin = fahrenheit + 457.87;
+      setKelvin(toKelvin);
+    }
+    if (event.target.id === "kelvin") {
+      const kelvin = event.target.value;
+      setKelvin(kelvin);
+
+      const toCelsius = parseFloat(kelvin) - 273.15;
+      setCelsius(toCelsius);
+
+      const toFahrenheit = kelvin - 457.87;
+      setFahrenheit(toFahrenheit);
+    }
+    console.log("Current change on. ", event.target.id);
+  }
 
   return (
     <>
@@ -19,41 +51,45 @@ function App() {
         <div className="type">
           <h2 className="name">Celsius °C:</h2>
           <TextField
-            id="standard-number"
+            id="celsius"
             label="°C"
             type="number"
             InputLabelProps={{
               shrink: true,
             }}
             variant="standard"
+            // defaultValue={0}
             /* Add values */
             value={celsius}
+            onChange={handleChange}
           />
         </div>
         <div className="type">
           <h2 className="name">Fahrenheit °F: </h2>
           <TextField
-            id="filled-number"
+            id="fahrenheit"
             label="°F"
             type="number"
             InputLabelProps={{
               shrink: true,
             }}
             variant="standard"
-            value={fahTemp}
+            value={fahrenheit}
+            onChange={handleChange}
           />
         </div>
         <div className="type">
           <h2 className="name">Kelvin °K:</h2>
           <TextField
-            id="standard-number"
+            id="kelvin"
             label="°K"
             type="number"
             InputLabelProps={{
               shrink: true,
             }}
             variant="standard"
-            value={kelTemp}
+            value={kelvin}
+            onChange={handleChange}
           />
         </div>
       </form>
